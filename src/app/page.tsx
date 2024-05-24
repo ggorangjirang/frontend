c21efe08e2cb779219467d589593a6072aced50b
[SW2/서울/1반] 강예정 <bananana0118@gmail.com>
2024-05-24 14:34:12 +0900

    feat : header, nav바 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import "./globals.css";
-import Header from "@/layout/Header";
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <div className="max-w-xl">
         <h1 className="underline">asdsadasdas</h1>
       </div>
