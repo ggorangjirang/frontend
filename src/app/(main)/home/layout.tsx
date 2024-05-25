a9386d891be5849643d1a3cf47144c24ee6e5f1e
[SW2/서울/1반] 강예정 <bananana0118@gmail.com>
2024-05-25 14:00:53 +0900

    feat : 부드럽게 사라지는 효과 추가

--- src/app/(main)/home/layout.tsx
+++ src/app/(main)/home/layout.tsx
@@ -15,7 +15,7 @@ export default function layout({ children }: Readonly<Props>) {
       <div className="relative">
         <Header setHover={setHover} />
         <div
-          className={`absolute top-full z-30 flex h-[8740px] w-full  transition-opacity delay-150 duration-300 ease-in-out ${hover ? "bg-black  opacity-75" : " bg-black  opacity-0 "} `}
+          className={`absolute top-full z-30 flex h-[8740px] w-full  transition-opacity delay-150 duration-300 ease-in-out ${hover ? "bg-black  opacity-75" : " bg-black  opacity-0 invisible "} `}
           style={{ transitionProperty: "opacity, visibility" }}
         ></div>
       </div>
