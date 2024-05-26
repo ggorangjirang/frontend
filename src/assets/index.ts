6344ad1811aebefa110c21d25f3b34f770266e72
[SW2/서울/1반] 강예정 <bananana0118@gmail.com>
2024-05-26 15:31:43 +0900

    feat : 오른쪽화살표 아이콘 추가 및 ButtonWithIcon 컴포넌트 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,3 +2,4 @@ export { default as Basket } from "@assets/icons/basket.svg";
 export { default as User } from "@assets/icons/user.svg";
 export { default as Search } from "@assets/icons/search.svg";
 export { default as Signup } from "@assets/icons/signup.svg";
+export { default as ArrowRight } from "@assets/icons/arrow-right.svg";
