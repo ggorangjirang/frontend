87ff6d3b8f771c40efb0f6da801f999bc0fe9fc6
[SW2/서울/1반] 강예정 <bananana0118@gmail.com>
2024-06-06 12:07:23 +0900

    feat : header 첫번째 baseUrl 경로로 헤더 표시 구분할 수 있게 수정

--- pagesConfig.ts
+++ pagesConfig.ts
@@ -6,39 +6,43 @@ type PageConfig = {
 };
 
 export const pageConfig: Record<string, PageConfig> = {
-  "/": {
+  "": {
     showHeader: true,
     title: "홈페이지",
   },
-  "/users": {
+  users: {
     showHeader: false,
     title: "유저 페이지",
   },
-  "/categories": {
+  categories: {
     showHeader: true,
     title: "카테고리 리스트 페이지",
   },
-  "/products": {
+  products: {
     showHeader: true,
     title: "상품 상세 페이지",
   },
-  "/mypage/info": {
+  mypage: {
     showHeader: true,
     title: "마이 페이지",
   },
-  "/mypage/view": {
-    showHeader: true,
-    title: "내 리뷰 조회 페이지",
-  },
-  "/mypage/write": {
-    showHeader: true,
-    title: "내 리뷰 작성 페이지",
-  },
-  "/buying": {
+  // "/mypage/view": {
+  //   showHeader: true,
+  //   title: "내 리뷰 조회 페이지",
+  // },
+  // "/mypage/write": {
+  //   showHeader: true,
+  //   title: "내 리뷰 작성 페이지",
+  // },
+  // "/mypage/purchased": {
+  //   showHeader: true,
+  //   title: "내 리뷰 작성 페이지",
+  // },
+  buying: {
     showHeader: true,
     title: "주문 페이지",
   },
-  "/bought": {
+  bought: {
     showHeader: true,
     title: "주문 완료 안내창 페이지",
   },
