441a15759be781492492c46e19bea67f2649d916
[SW2/서울/1반] 강예정 <bananana0118@gmail.com>
2024-06-09 17:15:23 +0900

    rename : 파일명 수정 및 이미지 추가

--- src/assets/index.mjs
+++ src/assets/index.mjs
@@ -1,4 +1,4 @@
-export { default as Basket } from "@assets/icons/basket.svg";
+export { default as Cart } from "@assets/icons/cart.svg";
 export { default as User } from "@assets/icons/user.svg";
 export { default as Search } from "@assets/icons/search.svg";
 export { default as Signup } from "@assets/icons/signup.svg";
