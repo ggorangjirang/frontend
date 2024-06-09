529456f48a6d54a9c3d67873cf190b71e2480239
[SW2/서울/1반] 강예정 <bananana0118@gmail.com>
2024-06-09 18:50:42 +0900

    build : 빌드를 위한 수정

--- src/app/(categories)/categories/layout.tsx
+++ src/app/(categories)/categories/layout.tsx
@@ -1,10 +1,12 @@
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import PageWrapper from "@/layout/Wrapper/PageWrapper";
 
 export default function CategoriesLayout({ children }: { children: ReactNode }) {
   return (
     <PageWrapper>
-      <div className="mt-8 flex w-full flex-row gap-8 ">{children}</div>
+      <Suspense>
+        <div className="mt-8 flex w-full flex-row gap-8 ">{children}</div>
+      </Suspense>
     </PageWrapper>
   );
 }
