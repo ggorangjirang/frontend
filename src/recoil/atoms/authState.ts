b236e3de28906bf207128db4262d82d0ac587a12
[SW2/서울/1반] 강예정 <bananana0118@gmail.com>
2024-06-14 14:50:42 +0900

    build : build test

--- src/recoil/atoms/authState.ts
+++ src/recoil/atoms/authState.ts
@@ -7,25 +7,25 @@ export const authState = atom({
   default: "",
 });
 
-const localStorageEffect =
-  (key: string): AtomEffect<string> =>
-  ({ setSelf, onSet }) => {
-    if (typeof window !== "undefined") {
-      const savedValue = window.localStorage.getItem(key);
-      if (savedValue !== null) {
-        setSelf(JSON.parse(savedValue));
-      }
-    }
-    //onSet => atom의 변화를 감지하고 구독한다.
-    onSet((newValue: string, _: string | DefaultValue, isReset: boolean) => {
-      isReset ? window.localStorage.removeItem(key) : window.localStorage.setItem(key, JSON.stringify(newValue));
-    });
-  };
+// const localStorageEffect =
+//   (key: string): AtomEffect<string> =>
+//   ({ setSelf, onSet }) => {
+//     if (typeof window !== "undefined") {
+//       const savedValue = window.localStorage.getItem(key);
+//       if (savedValue !== null) {
+//         setSelf(JSON.parse(savedValue));
+//       }
+//     }
+//     //onSet => atom의 변화를 감지하고 구독한다.
+//     onSet((newValue: string, _: string | DefaultValue, isReset: boolean) => {
+//       isReset ? window.localStorage.removeItem(key) : window.localStorage.setItem(key, JSON.stringify(newValue));
+//     });
+//   };
 
 export const tokenState = atom({
   key: "accessToken",
   default: "",
-  effects_UNSTABLE: [localStorageEffect("accessToken")],
+  // effects_UNSTABLE: [localStorageEffect("accessToken")],
 });
 
 export const userState = atom({
