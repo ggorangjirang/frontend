bef9a9640ae4b172537f4e10b0ea8df79708c5d1
[SW2/서울/1반] 강예정 <bananana0118@gmail.com>
2024-06-15 11:30:43 +0900

    feat : mypage merge

--- src/app/(mypage)/mypage/info/page.tsx
+++ src/app/(mypage)/mypage/info/page.tsx
@@ -46,6 +46,10 @@ const Page = () => {
       script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
       script.async = true;
       document.body.appendChild(script);
+
+      script.onload = () => {
+        console.log("Daum Postcode script loaded");
+      };
     }
   }, []);
 
@@ -61,6 +65,7 @@ const Page = () => {
       alert("전화번호 형식이 올바르지 않습니다. 올바른 형식: 010-1234-5678 또는 02-123-4567");
       return;
     }
+    console.log(zonecode);
     const response = await patchUser({
       ...data,
       name: user?.name,
@@ -71,26 +76,33 @@ const Page = () => {
         detailAddress: addressDetail,
       },
     });
+    console.log(response);
   };
   const onClickAddr = () => {
     if (window.daum && window.daum.Postcode) {
       new window.daum.Postcode({
         oncomplete: function (data: { address: string; zonecode: string }) {
+          console.log(data);
           setZonecode(data.zonecode);
           setAddress(data.address);
         },
       }).open();
     } else {
+      console.error("Daum Postcode API is not loaded.");
     }
   };
 
   useEffect(() => {
     const getUser = async () => {
-      const userInfo = await getUserInfoByEmail(token!);
-      setUser(userInfo);
-      setZonecode(userInfo?.address.zipcode);
-      setAddress(userInfo?.address.streetAddress);
-      setAddressDetail(userInfo?.address.detailAddress);
+      const token = localStorage.getItem("accessToken");
+      if (token) {
+        const userInfo = await getUserInfoByEmail(token!);
+        setUser(userInfo);
+        console.log(userInfo);
+        setZonecode(userInfo?.address.zipcode);
+        setAddress(userInfo?.address.streetAddress);
+        setAddressDetail(userInfo?.address.detailAddress);
+      }
     };
     getUser();
   }, [token]);
